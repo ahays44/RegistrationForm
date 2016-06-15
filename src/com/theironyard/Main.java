79617a82512576498f64940f68514ccd531963e7
austinhays <ahays.target@yahoo.com>
2016-06-15 14:54:36 -0400

    Add function added - not working

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,8 +1,69 @@
 package com.theironyard;
 
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+import org.h2.tools.Server;
+import spark.Spark;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void createTables(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, username VARCHAR, address VARCHAR, email VARCHAR)");
+    }
+
+    public static void insertUsers(Connection conn, String username, String address, String email) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO users VALUES (NULL, ?, ?, ?)");
+        stmt.setString(1, username);
+        stmt.setString(2, address);
+        stmt.setString(3, email);
+        stmt.execute();
+    }
+
+    public static ArrayList<User> selectUsers(Connection conn) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users");
+        ResultSet results = stmt.executeQuery();
+        ArrayList<User> users = new ArrayList<>();
+        while (results.next()) {
+            Integer id = results.getInt("id");
+            String username = results.getString("username");
+            String address = results.getString("address");
+            String email = results.getString("email");
+            User user = new User(id, username, address, email);
+            users.add(user);
+        }
+        return users;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Server.createWebServer().start();
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        createTables(conn);
+
+        Spark.externalStaticFileLocation("public");
+        Spark.init();
+
+        Spark.get(
+                "/get-messages",
+                (request, response) -> {
+                    ArrayList<User> users = selectUsers(conn);
+                    JsonSerializer s = new JsonSerializer();
+                    return s.serialize(users);
+                }
+        );
+
+        Spark.post(
+                "/add-message",
+                (request, response) -> {
+                    String body = request.body();
+                    JsonParser p = new JsonParser();
+                    User user = p.parse(body, User.class);
+                    insertUsers(conn, user.username, user.address, user.email);
+                    return "";
+                }
+        );
     }
 }
